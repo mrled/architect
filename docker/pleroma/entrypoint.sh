8712990eb32dea9b0ad4e7a11204fe1592d2d1d9
Micah R Ledbetter <me@micahrl.com>
2018-09-05 13:17:02 -0500

    WIP: Flesh out pleroma role more

--- docker/pleroma/entrypoint.sh
+++ docker/pleroma/entrypoint.sh
@@ -2,4 +2,9 @@
 
 set -eu
 
+PGPASSFILE="/run/secrets/superuser.pgpass" psql --file="/run/secrets/setup_db.psql"
+
+# TODO: preserve environment except for $HOME variable
+su - pleroma
+cd /pleroma
 mix phx.server
